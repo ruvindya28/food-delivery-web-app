75f8bf01bfcbccef10db1b5c3ef707af79206403
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-05-31 15:51:30 +0530

    update login page

---
+++ client/src/pages/AllProducts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useAppContext } from '../context/AppContext';
+
+const AllProducts = () => {
+
+    const {products} = useAppContext();
+  return (
+    <div className='mt-16 flex flex-col'>
+        <div>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default AllProducts
