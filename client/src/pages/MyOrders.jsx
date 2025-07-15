87d70f6b2f9f96e196d0735dbeb011c9832577e3
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-15 22:53:57 +0530

    refactor MyOrders component to improve item display and structure

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -32,12 +32,21 @@ const MyOrders = () => {
               <div>
                 <div className='flex items-center mb-4 md:mb-0'>
                   <div className='bg-primary/10 p-4 rounded-lg'>
-                     <img src='{item.product.image[0]}' alt='' className='w-16 h-16' />
+                     <img src={item.product.image[0]} alt='' className='w-16 h-16' />
                   </div>
-                  <div>
-                    <h2>{item.product.name}</h2>
+                  <div className='ml-4'>
+                    <h2 className='text-xl font-medium text-gray-800'>{item.product.name}</h2>
+                    <p>Category: {item.product.category}</p>
                     </div>
                 </div>
+                <div className='text-primary text-lg font-medium'>
+                  <p>Quantity: {item.quantity || "1"}</p>
+                  <p>Status: {order.status}</p>
+                  <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+                </div>
+                <p className='text-primary text-lg font-medium'>
+                  Amount: {currency}{item.product.offerPrice * item.quantity}
+                </p>
               </div>
               
             ))}
