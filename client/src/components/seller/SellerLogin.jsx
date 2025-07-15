bb787d9abdca8daaf03bd2aba9187e64d7e48e24
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-15 23:50:19 +0530

    implement SellerLogin component with authentication logic and navigation

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/AppContext';
 
 const SellerLogin = () => {
-  return (
-    <div>
-      
-    </div>
+    const {isSeller, setIsSeller,navigate} = useAppContext();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        if(isSeller){
+            navigate('/seller')
+        }
+    }, [isSeller])
+       
+
+  return !isSeller && (
+     <form onSubmit={onSubmitHandler}>
+
+     </form>
   )
 }
 
