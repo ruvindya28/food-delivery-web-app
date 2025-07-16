70cabfffc89b3f01cf57347c518881d53cd41cc7
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-17 00:27:45 +0530

    add seller-related components and update routing for seller dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,10 @@ import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 
 
@@ -21,7 +25,7 @@ const App = () => {
   const isSellerPath = useLocation().pathname.includes('seller');
   const {showUserLogin,isSeller}= useAppContext()
   return (
-    <div>
+    <div className='min-h-screen text-default text-gray-700 bg-white'>
       {isSellerPath ? null :  <Navbar/>}
       {showUserLogin ? <Login/> :null}
 
@@ -36,7 +40,11 @@ const App = () => {
         <Route path='/cart' element={<Cart />} />
         <Route path='/add-address' element={<AddAddress />} />
         <Route path='/my-orders' element={<MyOrders />} />
-        <Route path='/seller' element={isSeller ? null : <SellerLogin/>} />
+        <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+          <Route index element={isSeller ? <AddProduct/> : null} />
+          <Route path='product-list' element={<ProductList/>} />
+          <Route path='orders' element={<Orders/>} />
+        </Route>
       </Routes>
 
      </div>
