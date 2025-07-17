731cb81914041509aa825d00159b44d9479d48f9
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-17 19:52:47 +0530

    refactor Orders component to integrate context and improve order fetching; enhance styling and structure

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/appContext';
+import { assets } from '../../assets/assets';
 
 const Orders = () => {
+    const {currency} = useAppContext();
+    const [orders, setOrders] = useState([]);
+
+    const fetchOrders = async () => {
+        // Fetch orders from API and set state
+        setOrders(dummyOrders);
+    };
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
+
  return (
-        <div className="md:p-10 p-4 space-y-4">
+        <div className='no-scrollbar flex-1 h-[95vh] overflow-y-scroll'>
+          <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
                 <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
                     <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={boxIcon} alt="boxIcon" />
+                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
                                 <div key={index} className="flex flex-col justify-center">
                                     <p className="font-medium">
-                                        {item.product.name} <span className={`text-indigo-500 ${item.quantity < 2 && "hidden"}`}>x {item.quantity}</span>
+                                        {item.product.name} <span className="text-primary"> x{item.quantity}</span>
                                     </p>
                                 </div>
                             ))}
                         </>
                     </div>
 
-                    <div className="text-sm">
+                    <div className="text-sm md:text-base text-black/60">
                         <p className='font-medium mb-1'>{order.address.firstName} {order.address.lastName}</p>
                         <p>{order.address.street}, {order.address.city}, {order.address.state},{order.address.zipcode}, {order.address.country}</p>
                     </div>
@@ -33,6 +48,7 @@ const Orders = () => {
                     </div>
                 </div>
             ))}
+        </div>
         </div>
     );
 };
