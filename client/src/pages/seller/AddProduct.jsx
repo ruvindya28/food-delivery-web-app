4029a22256c5bfc695f878e5d8bc62181744d199
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-17 13:03:07 +0530

    refactor AddProduct and ProductList components for improved state management and UI; update SellerLayout for consistent styling

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { assets, categories } from '../../assets/assets'
 
 const AddProduct = () => {
 
@@ -27,8 +27,8 @@ const AddProduct = () => {
                                     const updatedFiles = [...files];
                                     updatedFiles[index] = e.target.files[0];
                                     setFiles(updatedFiles);
-                                }}
-                                type="file" id={`image${index}`} hidden />
+
+                                }} type="file" id={`image${index}`} hidden />
                                 <img className='max-w-24 cursor-pointer' src={files[index] ? URL.createObjectURL(files[index]) : assets.upload_area} alt="uploadArea" width={100} height={100} />
                             </label>
                         ))}
@@ -36,19 +36,18 @@ const AddProduct = () => {
                 </div>
                 <div className="flex flex-col gap-1 max-w-md">
                     <label className="text-base font-medium" htmlFor="product-name">Product Name</label>
-                    <input onChange={(e) => setName(e.target.value)} value={name}
-                     id="product-name" type="text" placeholder="Type here" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
+                    <input onChange={(e) => setName(e.target.value)} value={name} id="product-name" type="text" placeholder="Type here" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                 </div>
                 <div className="flex flex-col gap-1 max-w-md">
                     <label className="text-base font-medium" htmlFor="product-description">Product Description</label>
-                    <textarea onChange={(e) => setDescription(e.target.value)} value={description} id="product-description" rows={4} className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none" placeholder="Type here"></textarea>
+                    <textarea id="product-description" rows={4} className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none" placeholder="Type here"></textarea>
                 </div>
                 <div className="w-full flex flex-col gap-1">
                     <label className="text-base font-medium" htmlFor="category">Category</label>
                     <select onChange={(e) => setCategory(e.target.value)} value={category} id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
                         <option value="">Select Category</option>
-                        {[{ name: 'Electronics' }, { name: 'Clothing' }, { name: 'Accessories' }].map((item, index) => (
-                            <option key={index} value={item.name}>{item.name}</option>
+                        {categories.map((item,index) => (
+                            <option key={index} value={item.path}>{item.path}</option>
                         ))}
                     </select>
                 </div>
@@ -62,7 +61,7 @@ const AddProduct = () => {
                         <input onChange={(e) => setOfferPrice(e.target.value)} value={offerPrice} id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                 </div>
-                <button className="px-8 py-2.5 bg-primary text-white font-medium rounded">ADD</button>
+                <button className="px-8 py-2.5 bg-primary text-white font-medium rounded cursor-pointer">ADD</button>
             </form>
         </div>
     );
--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useAppContext } from '../../context/AppContext';
+
 import { assets } from '../../assets/assets';
 import { Link, NavLink, Outlet } from 'react-router-dom';
+import { useAppContext } from '../../context/appContext';
 
 const SellerLayout = () => {
 
@@ -32,7 +33,7 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className='flex'>
-                  <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                  <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
                 {sidebarLinks.map((item) => (
                    <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
                         className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
