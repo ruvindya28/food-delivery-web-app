1308e58ef76488ec667f1031128a2748c101d0e0
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-18 13:27:35 +0530

    Implement user registration with password hashing and JWT token generation; enhance error handling and response structure

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -14,11 +16,40 @@ export const register = async (req, res) => {
         }
 
         const existingUser = await User.findOne({ email });
-        if(existingUser) {
+        if(existingUser) 
             return res.json({success: false, message: 'User already exists' });
-        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
 
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+
+        });
+
+        return res.json({
+            success: true,
+            message: 'User registered successfully',
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        });   
+    
     }catch(error) {
-
+        res.json({
+            success: false,
+            message: error.message
+        });
     }
 }
