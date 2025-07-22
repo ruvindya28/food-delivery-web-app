53ccaa5eaf5e77786a978ecc491cde20b74a9317
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-23 00:08:21 +0530

    Add product management functionality with add, list, retrieve, and stock change endpoints

---
+++ server/controllers/productController.js
@@ -0,0 +1,27 @@
+
+//add products : /api/product/add
+export const addProduct = async (req, res) => {
+     
+
+}
+
+
+
+//get products : /api/product/list
+export const productList = async (req, res) => {
+     
+
+}
+
+
+//get single products : /api/product/id
+export const productById = async (req, res) => {
+     
+
+}
+
+//change products instock : /api/product/stock
+export const changeStock = async (req, res) => {
+     
+
+}
---
+++ server/models/Product.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+
+});
