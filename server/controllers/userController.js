c4bea87a6968190379ce17a46da8fd0f3d6324f1
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-22 10:57:13 +0530

    Implement logout functionality in userController and update user routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -126,3 +126,26 @@ export const isAuth = async (req, res) => {
 
 
 }
+
+//logout user : /api/user/logout
+
+export const logout = async (req, res) => {
+    try{
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        });
+
+        return res.json({
+            success: true,
+            message: 'User logged out successfully'
+        });
+    }catch(error) {
+        console.log(error.message);
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { login, register } from '../controllers/userController.js';
+import { isAuth, login, logout, register } from '../controllers/userController.js';
+import authUser from '../middleware/authUser.js';
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
+userRouter.post('/is-auth',authUser,isAuth);
+userRouter.get('/logout',authUser,logout);
 
 export default userRouter;
