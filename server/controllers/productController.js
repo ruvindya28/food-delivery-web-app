aa80632d00c0755b03bcd868fb7b4b4cc520e9fa
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-23 10:39:26 +0530

    Add error handling to product listing endpoint

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -38,7 +38,19 @@ export const addProduct = async (req, res) => {
 
 //get products : /api/product/list
 export const productList = async (req, res) => {
-     
+     try{
+            const products = await Product.find({});
+            res.json({
+                 success: true,
+                 products: products
+            });
+     }catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        });
+     }
  
 }
 
