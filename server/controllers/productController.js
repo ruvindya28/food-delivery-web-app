8800acde845ec3070e335a81d50c488950f82977
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-23 10:37:10 +0530

    Implement product addition functionality with image upload and error handling

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,36 @@
 import Product from '../models/Product.js';
+import {v2 as cloudinary} from 'cloudinary';
 //add products : /api/product/add
 export const addProduct = async (req, res) => {
-     
+     try{
+           let productData = JSON.parse(req.body.productData);
+
+           const images = req.files
+
+           let imagesUrl = await Promise.all(
+               images.map(async (item) => {
+                   let result = await cloudinary.uploader.upload(item.path, {
+                       resource_type: 'image',
+                   });
+                   return result.secure_url;
+               })
+           );
+           await Product.create({
+               ...productData,
+               image: imagesUrl,
+           })
+           res.json({
+                success: true,
+                message: 'Product added successfully',
+           })
+     }
+     catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        });
+     }
 
 }
 
