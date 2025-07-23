3ab9fdae3170ce5e31da3d676d9f1fa8bf2b90a1
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-07-23 10:45:28 +0530

    Add error handling to changeStock endpoint

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -76,6 +76,20 @@ export const productById = async (req, res) => {
 
 //change products instock : /api/product/stock
 export const changeStock = async (req, res) => {
-     
+     try{
+         const { id,inStock } = req.body;
+         await Product.findByIdAndUpdate(id, { inStock });
+         res.json({
+             success: true,
+             message: 'Stock updated successfully'
+         });
+
+     }catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        });
+     }
 
 }
