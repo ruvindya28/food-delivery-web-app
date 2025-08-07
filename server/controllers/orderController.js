a15553d24d2707ae546d21e24578bc7a62d9990d
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-08-07 23:25:58 +0530

    add the online payment gateway

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
+import stripe from "stripe" 
 
 //place order COD :/api/order/cod
 
@@ -20,7 +21,7 @@ export const placeOrderCOD = async (req, res) => {
             return (await acc) + (product.offerPrice * item.quantity);
          }, 0);
 
-         amount += Math.floor(amount * 0.02); // Adding 5% tax
+         amount += Math.floor(amount * 0.02); // Adding 2% tax
 
          await Order.create({
              userId,
@@ -30,13 +31,97 @@ export const placeOrderCOD = async (req, res) => {
              paymentType: "COD",
          // Create a new order
          });
-            res.json({
+          return res.json({
                 success: true,
                 message: "Order placed successfully",
             });
      } catch (error) {
          console.error(error.message);
-         res.json({
+        return res.json({
+             success: false,
+             message: error.message
+         });
+     }
+ }
+
+    //place order Strpe :/api/order/stripe
+   export const placeOrderStripe = async (req, res) => {
+     try {
+         const { userId, items, address } = req.body;
+         const {origin} = req.headers;
+
+         if(!address || items.lenght ===0){
+                return res.json({
+                    success: false,
+                    message: "Invalid data"
+                })
+         }
+
+         let productData = [];
+        
+
+         // Calculate total amount
+         let amount = await items.reduce(async (acc, item) => {
+
+            const product = await Product.findById(item.product);
+            productData.push({
+                name: product.name,
+                price: product.offerPrice,
+                quantity: item.quantity,
+            })
+            return (await acc) + (product.offerPrice * item.quantity);
+         }, 0);
+
+         amount += Math.floor(amount * 0.02); // Adding 2% tax
+
+         const order =await Order.create({
+             userId,
+             items,
+             address,
+             amount,
+             paymentType: "Online",
+         // Create a new order
+         });
+
+         // Stripe Gateway Initialize 
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+        //create line items for stripe 
+
+        const line_items = productData.map((item)=>{
+            return {
+                price_data: {
+                    currency: "LKR",
+                    product_data: {
+                        name: item.name,
+                    },
+                    unit_amount: Math.floor(item.price + item.price * 0.02) * 100
+                },
+                quantity: item.quantity
+            }
+        })
+
+        //create session 
+
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode: "payment",
+            success_url: `${origin}/loader?next=my-orders`,
+            cancel_url: `${origin}/cart`,
+            metadata:{
+                orderId: order._id.toString(),
+                userId,
+            }
+        })
+
+            
+          return res.json({
+                success: true,
+                url: session.url
+            });
+     } catch (error) {
+         console.error(error.message);
+        return res.json({
              success: false,
              message: error.message
          });
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import  authUser  from '../middleware/authUser.js';
 import authSeller from '../middleware/authSeller.js';
-import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/orderController.js';
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/cod', authUser, placeOrderCOD)
 orderRouter.get('/user', authUser, getUserOrders)
 orderRouter.get('/seller', authSeller, getAllOrders)
+orderRouter.post('/stripe', authUser, placeOrderStripe)
 
 
 export default orderRouter;
